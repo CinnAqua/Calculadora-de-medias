4775928f2fe80959e0ecf8e9446e9bfae6c3f35e
CinnAqua <miguelcp1008@gmail.com>
2023-07-18 20:40:31 -0300

    Arquivos para o site de agenda adicionados

--- main.js
+++ main.js
@@ -1,42 +1,33 @@
-const form = document.getElementById('formatividade');
-const imgAprovado = '<img src="images/aprovado.png" alt="Emoji celebrando" />'
-const imgReprovado = '<img src="images/reprovado.png" alt="Emoji decepcionado" />'
-const atividades = [];
-const notas = [];
-const spanAprovado = '<span class="resultado aprovado">Aprovado</Span>';
-const spanReprovado = '<span class="resultado reprovado">Reprovado</Span>';
-const notaMinima = parseFloat(prompt("Digite a nota mínima:"));
-
+const form = document.getElementById('formagenda');
+const nomes = [];
+const tels = [];
 let linhas = '';
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     adicionaLinha();
     atualizaTabela();
-    atualizaMediaFinal();
-});
+})
 
 function adicionaLinha() {
     const inputNome = document.getElementById('nome');
-    const inputNota = document.getElementById('nota');
+    const inputTel = document.getElementById('tel');
 
-    if (atividades.includes(inputNome.value)) {
-        alert(`A atividade: ${inputNome.value} Já foi inserida`);
+    if (nomes.includes(inputNome.value)||tels.includes(inputTel.value)) {
+        alert('Este nome ou número de contato já foi inserido');
     } else {
-        atividades.push(inputNome.value);
-        notas.push(parseFloat(inputNota.value));
+        nomes.push(inputNome.value);
+        tels.push(inputTel.value);
     
         let linha = `<tr>`;
         linha += `<td>${inputNome.value}</td>`;
-        linha += `<td>${inputNota.value}</td>`;
-        linha += `<td>${inputNota.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
+        linha += `<td><a href="tel:${inputTel.value}">${inputTel.value}</a></td>`;
         linha += `</tr>`;
-    
         linhas += linha;
+        inputNome.value = '';
+        inputTel.value = '';
     }
-
-    inputNome.value = '';
-    inputNota.value = '';
 }
 
 function atualizaTabela () {
@@ -44,19 +35,25 @@ function atualizaTabela () {
     corpoTabela.innerHTML = linhas;
 }
 
-function atualizaMediaFinal () {
-    const mediaFinal = calculaMediaFinal();
-
-    document.getElementById('mediafinalvalor').innerHTML = mediaFinal.toFixed(2);
-    document.getElementById('mediafinalresultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
+function mascara(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout("execmascara()",1)
 }
-
-function calculaMediaFinal() {
-    let somaNotas = 0;
-
-    for (let i = 0; i < notas.length; i++) {
-        somaNotas += notas[i];
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+function mtel(v){
+    v=v.replace(/\D/g,"");             //Remove tudo o que não é dígito
+    v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+    v=v.replace(/(\d)(\d{4})$/,"$1-$2");    //Coloca hífen entre o quarto e o quinto dígitos
+    return v;
+}
+function id( el ){
+    return document.getElementById( el );
+}
+window.onload = function(){
+    id('tel').onkeyup = function(){
+    mascara( this, mtel );
     }
-
-    return somaNotas / notas.length;
 }
